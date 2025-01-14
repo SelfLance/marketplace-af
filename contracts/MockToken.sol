101104bbd5890661fc9e2b2c3113d884494e89ea
Nasir <nasiralishigri786@gmail.com>
2025-01-15 01:19:33 +0500

    mock token added for testing

---
+++ contracts/MockToken.sol
@@ -0,0 +1,12 @@
+// contracts/MockToken.sol
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20 {
+    constructor() ERC20("MockToken", "MTK") {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
